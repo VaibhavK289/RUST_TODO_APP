dba5e1d2e325e8b23387def137906646c126094a
Vaibhav Kumar Kandhway <vaibhavkrkandhway@gmail.com>
2026-04-14 23:44:34 +0530

    Imp function started

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -2,3 +2,14 @@ fn main() {
     println!("Hello, world!");
 }
 // This is the main src file main code will be present here
+
+
+// Strict memory management and error handling will be implemented in this file. The main function will serve as the entry point for the application, and additional modules and functions will be defined to handle various functionalities of the todo CLI application.
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // we pass true as value
+        self.map.insert(key, true);
+    }
+}
